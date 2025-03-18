1f297a1a28a36ff68dcc73cbed3331f7a76c71b8
allyssasry <allyssasry@student.ub.ac.id>
2025-03-18 18:50:58 +0700

    Azure Update project

--- server.js
+++ server.js
@@ -1,11 +1,29 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => {
-    res.send('Hello, Saya Allyssa Kelas T4J');
+    res.send("Hello, Saya Allyssa");
 });
 
-const port = process.env.PORT || 3000;
+app.get('/form', (req, res) => {
+    res.send(`
+        <form method="post" action="/form">
+            Nama: <input type="text" name="name"><br>
+            Email: <input type="text" name="email"><br>
+            <input type="submit" value="Submit">
+        </form>
+    `);
+});
+
+app.post('/form', (req, res) => {
+    const { name, email } = req.body;
+    res.send(`Terima kasih, ${name}. Email Anda: ${email} telah disimpan.`);
+});
+
+const port = process.env.PORT || 5000;
 app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
+    console.log(`Server berjalan di http://localhost:${port}`);
 });
